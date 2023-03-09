d28e39988f5cf064ca26167baa6c14e9bca1cf0e
Aron Svastits <svastits1@gmail.com>
2023-03-09 09:33:13 +0100

    launch file convenctions

--- kuka_iisy_support/launch/test_lbr3r760.launch.py
+++ kuka_iisy_support/launch/test_lbr3r760.launch.py
@@ -12,35 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import yaml
+
 from launch import LaunchDescription
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-
-
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return file.read()
-    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
-        return None
-
-
-def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return yaml.safe_load(file)
-    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
-        return None
 
 
 def generate_launch_description():
@@ -59,8 +35,8 @@ def generate_launch_description():
     robot_description = {'robot_description': robot_description_content}
 
     # RViz
-    rviz_config_file = get_package_share_directory(
-        'kuka_iisy_support') + "/config/simple_urdf.rviz"
+    rviz_config_file = PathJoinSubstitution([FindPackageShare(
+        'kuka_iisy_support'), "config", "simple_urdf.rviz"])
     rviz_node = Node(package='rviz2',
                      executable='rviz2',
                      name='rviz2_launch',
