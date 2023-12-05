eeda875fdcd80c48930be3c7ee1c4852bb3494ac
Ravi Rathnam <ravi.rathnam@kuka.com>
2023-12-05 15:27:39 +0100

    added argument for quantec

--- kuka_omnimove_e575_support/launch/omnimove_with_quantec_launch.py
+++ kuka_omnimove_e575_support/launch/omnimove_with_quantec_launch.py
@@ -8,5 +8,5 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
   omnimove_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([FindPackageShare("kuka_omnimove_e575_support"), "/launch", "/omnimove_launch.py"]))
-  quantec_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([FindPackageShare("kuka_rsi_hw_interface"), "/launch", "/startup.launch.py"]))
+  quantec_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([FindPackageShare("kuka_rsi_hw_interface"), "/launch", "/startup.launch.py"]), launch_arguments={'robot_model': 'kr210_r2700-2'}.items())
   return LaunchDescription([omnimove_launch, quantec_launch])
