7905f734b337694bffa545bd54581c1993e2fe10
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-01-29 13:08:41 +0100

    using new caterpillar and omnimove model publishing transform between platform and robot

--- kuka_omnimove_e575_support/launch/caterpillar_with_iontec.py
+++ kuka_omnimove_e575_support/launch/caterpillar_with_iontec.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-import launch_ros.actions
+from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 
@@ -9,4 +9,8 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
   omnimove_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([FindPackageShare("kuka_omnimove_e575_support"), "/launch", "/caterpillar_launch.py"]))
   iontec_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([FindPackageShare("kuka_kss_rsi_driver"), "/launch", "/startup.launch.py"]))
-  return LaunchDescription([omnimove_launch, iontec_launch])
+  transform_publisher = LaunchDescription([Node(package='tf2_ros', executable='static_transform_publisher',
+                                                arguments=[
+                                                  '--x', '0.559', '--y', '0.00', '--z', '0.674', '--frame-id',
+                                                  'caterpillar_base', '--child-frame-id', 'base_link'])])
+  return LaunchDescription([omnimove_launch, iontec_launch, transform_publisher])
--- kuka_omnimove_e575_support/launch/omnimove_with_quantec_launch.py
+++ kuka_omnimove_e575_support/launch/omnimove_with_quantec_launch.py
@@ -1,12 +1,17 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-import launch_ros.actions
+from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 
 
-
 def generate_launch_description():
-  omnimove_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([FindPackageShare("kuka_omnimove_e575_support"), "/launch", "/omnimove_launch.py"]))
-  quantec_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([FindPackageShare("kuka_kss_rsi_driver"), "/launch", "/startup.launch.py"]), launch_arguments={'robot_model': 'kr210_r2700_2', 'robot_family' : 'quantec'}.items())
-  return LaunchDescription([omnimove_launch, quantec_launch])
+    omnimove_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(
+        [FindPackageShare("kuka_omnimove_e575_support"), "/launch", "/omnimove_launch.py"]))
+    quantec_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([FindPackageShare("kuka_kss_rsi_driver"), "/launch", "/startup.launch.py"]),
+        launch_arguments={'robot_model': 'kr210_r2700_2', 'robot_family': 'quantec'}.items())
+    transform_publisher = LaunchDescription([Node(package='tf2_ros', executable='static_transform_publisher',
+                                                  arguments=[
+                                                      '--x', '0.89567',  '--y', '0.00247', '--z', '0.48018', '--frame-id', 'omnimove_base', '--child-frame-id', 'base_link'])])
+    return LaunchDescription([omnimove_launch, quantec_launch, transform_publisher])
