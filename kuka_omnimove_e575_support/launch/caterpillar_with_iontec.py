4aa328a4807e4dbac0307d801b2ddf7b9f91fbcf
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-01-29 13:10:27 +0100

    formatting

--- kuka_omnimove_e575_support/launch/caterpillar_with_iontec.py
+++ kuka_omnimove_e575_support/launch/caterpillar_with_iontec.py
@@ -5,12 +5,13 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 
 
-
 def generate_launch_description():
-  omnimove_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([FindPackageShare("kuka_omnimove_e575_support"), "/launch", "/caterpillar_launch.py"]))
-  iontec_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([FindPackageShare("kuka_kss_rsi_driver"), "/launch", "/startup.launch.py"]))
-  transform_publisher = LaunchDescription([Node(package='tf2_ros', executable='static_transform_publisher',
-                                                arguments=[
-                                                  '--x', '0.559', '--y', '0.00', '--z', '0.674', '--frame-id',
-                                                  'caterpillar_base', '--child-frame-id', 'base_link'])])
-  return LaunchDescription([omnimove_launch, iontec_launch, transform_publisher])
+    omnimove_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(
+        [FindPackageShare("kuka_omnimove_e575_support"), "/launch", "/caterpillar_launch.py"]))
+    iontec_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([FindPackageShare("kuka_kss_rsi_driver"), "/launch", "/startup.launch.py"]))
+    transform_publisher = LaunchDescription([Node(package='tf2_ros', executable='static_transform_publisher',
+                                                  arguments=[
+                                                      '--x', '0.559', '--y', '0.00', '--z', '0.674', '--frame-id',
+                                                      'caterpillar_base', '--child-frame-id', 'base_link'])])
+    return LaunchDescription([omnimove_launch, iontec_launch, transform_publisher])
