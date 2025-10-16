f8c51aba09dab53d25a8bbabfb3f3127d9ad17bf
Karnitscher <karnitschervilmos@edu.bme.hu>
2025-10-16 14:54:53 +0200

    hook script fixes

--- .ci_config/hooks/before_run_target_test.sh
+++ .ci_config/hooks/before_run_target_test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "Running before run test target hook Gazebo test script..."
+source /opt/ros/$ROS_DISTRO/setup.bash
+source install/setup.bash
 ros2 run kuka_gazebo run_gazebo_tests.py
