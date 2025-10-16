8d7c5a5a3fd18183a6db917df84826b7338d4265
Karnitscher <karnitschervilmos@edu.bme.hu>
2025-10-16 16:39:09 +0200

    Python script istead of ros2 run

--- .ci_config/hooks/before_run_target_test.sh
+++ .ci_config/hooks/before_run_target_test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "Running before run test target hook Gazebo test script..."
-source /opt/ros/jazzy/setup.bash
-ros2 run kuka_gazebo run_gazebo_tests.py
+python3 install/kuka_gazebo/lib/kuka_gazebo/run_gazebo_tests.py
