065a1f8f4d55530bfdbddcb354929e80da34f48d
Karnitscher <karnitschervilmos@edu.bme.hu>
2025-10-16 16:18:24 +0200

    ici hook ros fix

--- .ci_config/hooks/before_run_target_test.sh
+++ .ci_config/hooks/before_run_target_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Running before run test target hook Gazebo test script..."
-colocon build
-source install/setup.bash
+source /opt/ros/jazzy/setup.bash
 ros2 run kuka_gazebo run_gazebo_tests.py
