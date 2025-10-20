f9d11d53b8276ea5ca723dcb9266c00bddf1cf6e
Karnitscher <karnitschervilmos@edu.bme.hu>
2025-10-20 14:31:33 +0200

    more detailed logging for error recognition

--- .ci_config/hooks/before_run_target_test.sh
+++ .ci_config/hooks/before_run_target_test.sh
@@ -3,7 +3,20 @@ echo "Running before run test target hook Gazebo test script..."
 ls
 echo $ROS_DISTRO
 cd kuka_gazebo
+echo "kuka_gazebo"
 ls
+cd src
+echo "src"
+ls
+cd ..
+cd launch
+echo "launch"
+ls
+cd ..
+cd test
+echo "test"
+ls
+cd ..
 #chmod +x src/run_gazebo_tests.py
 cd ..
 source /opt/ros/$ROS_DISTRO/setup.bash
