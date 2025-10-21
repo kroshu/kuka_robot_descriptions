8dd7d3bdd1fee60ec6cf6775d9f2e5500d97c47a
Karnitscher <karnitschervilmos@edu.bme.hu>
2025-10-21 10:53:16 +0200

    trying to set up a virtual display

--- .ci_config/hooks/before_run_target_test.sh
+++ .ci_config/hooks/before_run_target_test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 echo "Running before run test target hook Gazebo test script..."
+sudo apt-get install -y libxcb-xinerama0
 Xvfb :99 -screen 0 1024x768x24 &
 XVFB_PID=$!
 sleep 2
 export DISPLAY=:99
-echo "virtual dsplay set"
+export QT_QPA_PLATFORM=xcb
+export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/qt/plugins/platforms  # Optional
+echo "Virtual display set"
 cd ~/target_ws
 ls
 source install/setup.bash
-ros2 launch kuka_gazebo gazebo.launch.py
 ros2 run kuka_gazebo run_gazebo_tests.py
+kill $XVFB_PID
