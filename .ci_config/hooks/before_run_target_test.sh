fc5b7da870fc4ad659e4e82f426c0b9056d988eb
Karnitscher <karnitschervilmos@edu.bme.hu>
2025-10-21 09:11:45 +0200

    execution in target ws

--- .ci_config/hooks/before_run_target_test.sh
+++ .ci_config/hooks/before_run_target_test.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
 echo "Running before run test target hook Gazebo test script..."
 ls
-echo $ROS_DISTRO
-cd kuka_gazebo
-echo "kuka_gazebo"
+cd ~/target_ws
 ls
-cd src
-echo "src"
-ls
-cd ..
-cd launch
-echo "launch"
-ls
-cd ..
-cd test
-echo "test"
-ls
-cd ..
-#chmod +x src/run_gazebo_tests.py
-cd ..
-cd ..
-ls
-cd ..
-ls
-source /opt/ros/$ROS_DISTRO/setup.bash
+source install/setup.bash
+#source /opt/ros/$ROS_DISTRO/setup.bash
 ros2 launch kuka_gazebo gazebo.launch.py
 ros2 run kuka_gazebo run_gazebo_tests.py
