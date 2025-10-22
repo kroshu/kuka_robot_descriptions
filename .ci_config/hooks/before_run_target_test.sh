1ca72ff3fcb17964c1bed5fca240667e9d75db03
Karnitscher <karnitschervilmos@edu.bme.hu>
2025-10-22 10:13:55 +0200

    added bridge config yaml

--- .ci_config/hooks/before_run_target_test.sh
+++ .ci_config/hooks/before_run_target_test.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 echo "Running before run test target hook Gazebo test script..."
-# sudo apt-get install -y libxcb-xinerama0
-# Xvfb :99 -screen 0 1024x768x24 &
-# XVFB_PID=$!
-# sleep 2
-# export DISPLAY=:99
-# export QT_QPA_PLATFORM=xcb
-# export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/qt/plugins/platforms  # Optional
-# echo "Virtual display set"
 cd ~/target_ws
+sudo apt install ros-jazzy-ros2-control ros-jazzy-ros2-controllers
 ls
 source install/setup.bash
 ros2 run kuka_gazebo run_gazebo_tests.py
