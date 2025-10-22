2366b9f36b94e612879f93d5e5fd784b90b1c698
Karnitscher <karnitschervilmos@edu.bme.hu>
2025-10-22 09:12:09 +0200

    Virtual display through CI

--- .ci_config/hooks/before_run_target_test.sh
+++ .ci_config/hooks/before_run_target_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Running before run test target hook Gazebo test script..."
-sudo apt-get install -y libxcb-xinerama0
-Xvfb :99 -screen 0 1024x768x24 &
-XVFB_PID=$!
-sleep 2
-export DISPLAY=:99
-export QT_QPA_PLATFORM=xcb
-export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/qt/plugins/platforms  # Optional
-echo "Virtual display set"
+# sudo apt-get install -y libxcb-xinerama0
+# Xvfb :99 -screen 0 1024x768x24 &
+# XVFB_PID=$!
+# sleep 2
+# export DISPLAY=:99
+# export QT_QPA_PLATFORM=xcb
+# export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/qt/plugins/platforms  # Optional
+# echo "Virtual display set"
 cd ~/target_ws
 ls
 source install/setup.bash
